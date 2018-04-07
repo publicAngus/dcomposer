e31e1136807b2d0f78f7260dbce5ac930bf204d8
angus <angus@virtualBox.ubuntu14-04>
2018-04-07 12:49:53 +0800

    added setup files

---
+++ setup.1.sh
@@ -0,0 +1,21 @@
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+   add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   apt-get update
+   apt-get install docker-ce
+
+   curl -L https://github.com/docker/compose/releases/download/1.20.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+   chmod +x /usr/local/bin/docker-compose
+
+   add-apt-repository ppa:certbot/certbot
+   apt-get update
+   apt-get install certbot
---
+++ setup.sh
@@ -0,0 +1,21 @@
+
+sudo apt-get update
+
+sudo apt-get install git
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+   sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   sudo apt-get update
+   sudo apt-get install docker-ce
+
+   sudo curl -L https://github.com/docker/compose/releases/download/1.20.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
